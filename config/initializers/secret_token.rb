f164e02e022399b37fb2b45fe81fa581e1bac290
vagrant <vagrant@precise64.(none)>
2013-07-18 17:59:57 +0000

    BetandGoal commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetandGoal::Application.config.secret_key_base = '1abbf69c148999043a8afd0d2ef8571b33775305f28487df835c066327f6c8071a0f68e20310d68c29a9412b3b795857a7893f9352b9d0e9f533d0dffd183e92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetandGoal::Application.config.session_store :cookie_store, key: '_BetandGoal_session'
